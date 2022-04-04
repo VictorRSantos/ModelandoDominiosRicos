c2e463727b88d14a71beb083853c54b800a70756
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-04-04 10:41:40 -0300

    Utilizando biblioteca Flunt e Design By Contracts

--- PaymentContext/PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext/PaymentContext.Domain/Entities/Payment.cs
@@ -1,15 +1,17 @@
 
+using Flunt.Validations;
 using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Entities;
 
 namespace PaymentContext.Domain.Entities
 {
 
-    public abstract class Payment
+    public abstract class Payment : Entity
     {
         protected Payment(DateTime paiDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Document document, Address address, Email email)
         {
             Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 10).ToUpper();
-            PaiDate = paiDate;
+            PaidDate = paiDate;
             ExpireDate = expireDate;
             Total = total;
             TotalPaid = totalPaid;
@@ -17,10 +19,16 @@ namespace PaymentContext.Domain.Entities
             Document = document;
             Address = address;
             Email = email;
+
+            AddNotifications(new Contract()
+                .Requires()
+                .IsGreaterThan(0, Total, "Payment.Total", "O total não pode ser zero")
+                .IsGreaterOrEqualsThan(Total, TotalPaid, "Payment.TotalPaid", "O valor pago é menor que o valor do pagamento")
+            );
         }
 
         public string Number { get; private set; }
-        public DateTime PaiDate { get; private set; }
+        public DateTime PaidDate { get; private set; }
         public DateTime ExpireDate { get; private set; }
         public decimal Total { get; private set; }
         public decimal TotalPaid { get; private set; }
--- PaymentContext/PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext/PaymentContext.Domain/Entities/Student.cs
@@ -1,3 +1,4 @@
+using Flunt.Validations;
 using PaymentContext.Domain.ValueObjects;
 using PaymentContext.Shared.Entities;
 
@@ -29,12 +30,23 @@ namespace PaymentContext.Domain.Entities
         {
 
 
-            foreach (var sub in Subscriptions)
-                sub.Inactivate();
+            var hasSubscriptionActive = false;
 
+            foreach (var sub in _subscriptions)
+            {
+                if (sub.Active)
+                    hasSubscriptionActive = true;
 
-            _subscriptions.Add(subscription);
+            }
 
+            // AddNotifications(new Contract()
+            //     .Requires()
+            //     .IsFalse(hasSubscriptionActive, "Student.Subscriptions", "Você já tem uma assinatura ativa")
+            // );
+
+            //Alternativa
+            if (hasSubscriptionActive)
+                AddNotification("Student.Subscriptions", "Você já tem uma assinatura ativa");
 
         }
     }
